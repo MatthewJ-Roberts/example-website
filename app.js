4f5811b8c5327288ec73c49cb1dcb1dc61c9b402
Matthew Roberts <matthewjroberts2001@gmail.com>
2021-04-19 16:19:32 -0300

    Changed default website theme to dark.

--- app.js
+++ app.js
@@ -3,14 +3,14 @@
 const switcher = document.querySelector('.btn');
 
 switcher.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme')
+    document.body.classList.toggle('light-theme')
 
     var className = document.body.className;
-    if (className == "light-theme") {
-        this.textContent = "Dark";
+    if (className == "dark-theme") {
+        this.textContent = "Light";
     }
     else {
-        this.textContent = "Light";
+        this.textContent = "Dark";
     }
     console.log('current class name: ' + className);
 })
